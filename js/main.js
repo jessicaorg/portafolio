f8fc0e5cf38d060459e11234b74fbd2eb310a894
Jessica <jessiica_om97@hotmail.com>
2023-10-16 11:50:10 -0600

    Mi portafolio

---
+++ js/main.js
@@ -0,0 +1,112 @@
+(function ($) {
+    "use strict";
+
+    // Navbar on scrolling
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
+
+
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var targetOffset = $(this.hash).offset();
+            
+            if (targetOffset) {
+                $('html, body').animate({
+                    scrollTop: targetOffset.top - 45
+                }, 1500, 'easeInOutExpo');
+
+                if ($(this).parents('.navbar-nav').length) {
+                    $('.navbar-nav .active').removeClass('active');
+                    $(this).closest('a').addClass('active');
+                }
+            }
+        }
+    });
+
+
+    // Texto dinamico
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+
+
+    // Animación de boton ir abajo
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-to-bottom').fadeOut('slow');
+        } else {
+            $('.scroll-to-bottom').fadeIn('slow');
+        }
+    });
+
+
+    // Filtro portafolio
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+    
+    
+    // Regresar arriba
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // skills carrousel
+    // $(".testimonial-carousel").owlCarousel({
+    //     autoplay: true,
+    //     smartSpeed: 1500,
+    //     dots: true,
+    //     loop: true,
+    //     items: 5
+    // });
+    
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            768: {
+                items: 3 
+            },
+            992: {
+                items: 5 
+            }
+        }
+    });
+})(jQuery);
+
